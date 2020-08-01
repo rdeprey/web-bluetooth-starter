f2ccd7b22277253be8de2801dde75e5761840185
Rebecca Deprey <rdeprey@gmail.com>
2020-08-01 19:25:38 -0400

    Updates

---
+++ precache-manifest.570a3ad652a582b0a1c885726e07c159.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45d86f8cf68dceb71f28584c799fe618",
+    "url": "/web-bluetooth-starter/index.html"
+  },
+  {
+    "revision": "ffcc869121c95460d5e8",
+    "url": "/web-bluetooth-starter/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0ae72ffa0fada5bfda98",
+    "url": "/web-bluetooth-starter/static/js/2.1cf166ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/web-bluetooth-starter/static/js/2.1cf166ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffcc869121c95460d5e8",
+    "url": "/web-bluetooth-starter/static/js/main.2d5edb9b.chunk.js"
+  },
+  {
+    "revision": "5d653af5544c8dad2fc4",
+    "url": "/web-bluetooth-starter/static/js/runtime-main.92ac76a8.js"
+  }
+]);
